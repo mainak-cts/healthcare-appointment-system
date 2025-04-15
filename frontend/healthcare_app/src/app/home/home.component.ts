455b45aa12541dcf3af707183a0bb03a6775d319
mainak-cts <Mainak.Mukherjee5@cognizant.com>
2025-04-15 23:21:07 +0530

    Removed unused imports

--- frontend/healthcare_app/src/app/home/home.component.ts
+++ frontend/healthcare_app/src/app/home/home.component.ts
@@ -1,8 +1,7 @@
-import { Component, inject, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import {MatIconModule} from '@angular/material/icon';
 import {MatDividerModule} from '@angular/material/divider';
 import {MatButtonModule} from '@angular/material/button';
-import { AuthApiService } from '../../services/authapi.service';
 import { ToastManagerService } from '../../services/toastr.service';
 import { ToastrService } from 'ngx-toastr';
 @Component({
